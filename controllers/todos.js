fac42771fd98209eec758b3223d2d5d36df077e2
dmalkan <dsmalkan@gmail.com>
2022-09-04 18:19:56 -0400

    removed an error checker consolelog

--- controllers/todos.js
+++ controllers/todos.js
@@ -15,7 +15,6 @@ module.exports = {
                 // added the tail portion to give the db object the day to store
         try{
             await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id, day: req.body.day})
-            console.log(req.body.day)
             console.log('Todo has been added!')
             res.redirect('/todos')
         }catch(err){
